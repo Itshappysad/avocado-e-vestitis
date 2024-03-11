6a231a4bb45769a3cc5a75f6de7e8cdfbe84a47d
NexmoCompanysisas <156712037+NexmoCompanysisas@users.noreply.github.com>
2024-03-11 14:22:25 -0500

    Delete Avocado-e-vestiti-main directory

--- Avocado-e-vestiti-main/src/core/types.ts
+++
@@ -1,7 +0,0 @@
-export type ResgisterUser = {
-  name: string;
-  email: string;
-  password?: string | null;
-  id: string;
-  provider?: string | null;
-};
--- Avocado-e-vestiti-main/src/pages/root.tsx
+++
@@ -1,9 +0,0 @@
-function Root() {
-  return (
-    <>
-      <div className='text-2xl'>Root</div>
-      <main></main>
-    </>
-  );
-}
-export default Root;
